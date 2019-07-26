ddde93a4d91d1377f0ac2c8f21e2ec12a1f21fb9
douglasgp <douglasgaldino22@hotmail.com>
2019-07-25 22:18:59 -0300

    Classe teste para passagem de conteúdo através de métodos OO

--- src/br/com/review/test/TestMain.java
+++ src/br/com/review/test/TestMain.java
@@ -17,14 +17,13 @@ public class TestMain {
 		int totalDisciplina = GeradorDatas.retornaTotalDisciplinas(dis.getSemestre());
 		dis.setTotalDiscSemestre(totalDisciplina);
 		
-		int cargaHorariaSemestre[] = GeradorDatas.cargaHorariaSemestre(dis.getSemestre());
-		dis.setCargaHoraria(cargaHorariaSemestre);
-		
+		int cargaHoraria[] = GeradorDatas.cargaHorariaSemestre(dis.getSemestre());
+				
 		System.out.println("Semestre: " + dis.getSemestre());
 		System.out.println("Qtd disciplinas: " + dis.getTotalDiscSemestre());
 		System.out.println("Carga horária");
 		for (int i = 0; i < dis.getTotalDiscSemestre(); i++) {
-			System.out.println((i+1)+"º. " + dis.getCargaHoraria()[i]);
+			System.out.println(cargaHoraria[i]);
 		}
 	}
 }
