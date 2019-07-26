1c09cfe007f47320f0fbf1efaaab396f4771b4cf
douglasgp <douglasgaldino22@hotmail.com>
2019-07-26 13:56:50 -0300

    Tentativa de correção no envio e busca da variável externa

--- src/br/com/review/test/TestaObjetoVetor.java
+++ src/br/com/review/test/TestaObjetoVetor.java
@@ -2,7 +2,8 @@ package br.com.review.test;
 
 import java.util.Scanner;
 
-import br.com.review.model.Disciplinas;
+import br.com.review.controller.GeradorDatas;
+import br.com.review.model.*;
 
 public class TestaObjetoVetor {
 	public static Scanner scan = new Scanner(System.in);
@@ -10,7 +11,21 @@ public class TestaObjetoVetor {
 	
 	public static void main(String[] args) {
 		System.out.println("Semestre?");
+		// Variável armazena num do semestre
 		dis.setSemestre(scan.nextInt());
+		// Variável armazena a quantidade de disciplinas do semestre informado
+		dis.setTotalDiscSemestre(GeradorDatas.retornaTotalDisciplinas(dis.getSemestre()));
+				
+		dis.setCargaHoraria(GeradorDatas.cargaHorariaSemestre(dis.getSemestre()));
+		
+		for (int i = 0; i < dis.getTotalDiscSemestre(); i++) {
+			dis.getCargaHoraria();
+		}
+		System.out.println(dis.getSemestre() + "º Semestre");
+		System.out.println("Qtd disciplinas: " + dis.getTotalDiscSemestre());
+		
+		for (int i = 0; i < dis.getTotalDiscSemestre(); i++) {
+			System.out.println((i+1) + "º ");
+		}
 	}
-
 }
