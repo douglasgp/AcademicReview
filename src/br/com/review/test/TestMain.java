a1738278a0f5f6e58aeb61d5ed2d27a4012efd35
douglasgp <douglasgaldino22@hotmail.com>
2019-07-27 17:07:47 -0300

    Remoção de códigos
    Organização de código definindo tarefas de classes

--- src/br/com/review/test/TestMain.java
+++ src/br/com/review/test/TestMain.java
@@ -1,29 +1,5 @@
 package br.com.review.test;
 
-import java.util.Scanner;
-
-import br.com.review.controller.GeradorDatas;
-import br.com.review.model.Disciplinas;
-
 public class TestMain {
 	
-	public static Scanner scan =new Scanner(System.in);
-	public static Disciplinas dis = new Disciplinas();
-	
-	public static void main(String[] args) {
-		System.out.println("Informe semestre: ");
-		dis.setSemestre(scan.nextInt());
-		
-		int totalDisciplina = GeradorDatas.retornaTotalDisciplinas(dis.getSemestre());
-		dis.setTotalDiscSemestre(totalDisciplina);
-		
-		int cargaHoraria[] = GeradorDatas.cargaHorariaSemestre(dis.getSemestre());
-				
-		System.out.println("Semestre: " + dis.getSemestre());
-		System.out.println("Qtd disciplinas: " + dis.getTotalDiscSemestre());
-		System.out.println("Carga horária");
-		for (int i = 0; i < dis.getTotalDiscSemestre(); i++) {
-			System.out.println(cargaHoraria[i]);
-		}
-	}
 }
