f7275961dc6f1d97ac9d781beb0b2ea9adec35fb
douglasgp <douglasgaldino22@hotmail.com>
2019-07-27 17:08:17 -0300

    Tentativa de retorno em método externo (INCOMPLETO)

--- src/br/com/review/test/TestaObjetoVetor.java
+++ src/br/com/review/test/TestaObjetoVetor.java
@@ -7,25 +7,21 @@ import br.com.review.model.*;
 
 public class TestaObjetoVetor {
 	public static Scanner scan = new Scanner(System.in);
-	public static Disciplinas dis = new Disciplinas();
 	
 	public static void main(String[] args) {
 		System.out.println("Semestre?");
 		// Variável armazena num do semestre
-		dis.setSemestre(scan.nextInt());
-		// Variável armazena a quantidade de disciplinas do semestre informado
-		dis.setTotalDiscSemestre(GeradorDatas.retornaTotalDisciplinas(dis.getSemestre()));
-				
-		dis.setCargaHoraria(GeradorDatas.cargaHorariaSemestre(dis.getSemestre()));
+		int semestre = scan.nextInt();
 		
-		for (int i = 0; i < dis.getTotalDiscSemestre(); i++) {
-			dis.getCargaHoraria();
-		}
-		System.out.println(dis.getSemestre() + "º Semestre");
-		System.out.println("Qtd disciplinas: " + dis.getTotalDiscSemestre());
+		int totalDisciplina = GeradorDatas.retornaTotalDisciplinas(semestre);
+		int[] cargaHoraria = GeradorDatas.cargaHorariaSemestre(semestre);
+		String[] nomeDisciplinas = GeradorDatas.disciplinasSemestral(semestre);
 		
-		for (int i = 0; i < dis.getTotalDiscSemestre(); i++) {
-			System.out.println((i+1) + "º ");
+		System.out.println(semestre + "º Semestre");
+		System.out.println("Qtd disciplinas: " + totalDisciplina);
+		
+		for (int i = 0; i < cargaHoraria.length; i++) {
+			System.out.println((i+1) + "º. " + nomeDisciplinas[i] + " - " + cargaHoraria[i]);
 		}
 	}
 }
