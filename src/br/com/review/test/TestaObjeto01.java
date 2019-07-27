651837529cebc7beedef6e73e9e6860c891c0702
douglasgp <douglasgaldino22@hotmail.com>
2019-07-27 17:23:21 -0300

    Método retorna vetor de nomes
    Implementação e correção de método externo, com retorno de vetor de
    string contendo nomes das disciplinas

--- src/br/com/review/test/TestaObjeto01.java
+++ src/br/com/review/test/TestaObjeto01.java
@@ -8,6 +8,7 @@ import br.com.review.model.Disciplinas;
 
 public class TestaObjeto01 {
 	public static Scanner scan = new Scanner(System.in);
+	public static int TOTAL_HORAS_SEMESTRE = 0;
 
 	public static void main(String[] args) {
 		System.out.println("Informe o semestre: ");
@@ -15,13 +16,16 @@ public class TestaObjeto01 {
 
 		int totalDiscSemestre = MethodsReview.retornaTotalDisciplinas(semestre);
 
-		int horasDiscSemestre[] = MethodsReview.cargaHorariaSemestre(semestre, totalDiscSemestre);
+		int[] horasDiscSemestre = MethodsReview.cargaHorariaSemestre(semestre, totalDiscSemestre);
+
+		String[] nomeDiscSemestre = MethodsReview.disciplinasSemestral(semestre, totalDiscSemestre);
 
 		System.out.println("Qtd disciplinas: " + totalDiscSemestre);
 		for (int i = 0; i < horasDiscSemestre.length; i++) {
-			System.out.println(" = = = " + (i + 1) + " disciplina = = = = ");
-			System.out.println((i + 1) + ". " + horasDiscSemestre[i]);
+			System.out.println((i + 1) + ". " + nomeDiscSemestre[i] + " - " + horasDiscSemestre[i]);
+			TOTAL_HORAS_SEMESTRE += horasDiscSemestre[i];
 		}
+		System.out.println("Total Horas: " + TOTAL_HORAS_SEMESTRE);
 
 	}
 
