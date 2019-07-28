45f92ba504cf6aaaf8441a12d9942d6d5a595295
douglasgp <douglasgaldino22@hotmail.com>
2019-07-28 12:51:56 -0300

    Primeira fase de implementação de bibliografia de disciplinas
    Criação de vetores contendo descrição das disciplinas em suas
    respectivas posições

--- src/br/com/review/test/TestaObjeto01.java
+++ src/br/com/review/test/TestaObjeto01.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 import br.com.review.controller.GeradorDatas;
 import br.com.review.controller.MethodsReview;
+import br.com.review.model.Bibliografia;
 import br.com.review.model.Disciplinas;
 
 public class TestaObjeto01 {
 	public static Scanner scan = new Scanner(System.in);
-
+	public static int contBiblio = 0;
 	public static void main(String[] args) {
 		System.out.println("Informe o semestre: ");
 		int semestre = scan.nextInt();
@@ -16,11 +17,15 @@ public class TestaObjeto01 {
 		int totalDiscSemestre = MethodsReview.retornaTotalDisciplinas(semestre);
 
 		int horasDiscSemestre[] = MethodsReview.cargaHorariaSemestre(semestre, totalDiscSemestre);
-
+		
+		String[] nomesDisc = MethodsReview.disciplinasSemestral(semestre, totalDiscSemestre);
+		
+		Bibliografia[] biblio = new Bibliografia[contBiblio];
+		
+		
 		System.out.println("Qtd disciplinas: " + totalDiscSemestre);
 		for (int i = 0; i < horasDiscSemestre.length; i++) {
-			System.out.println(" = = = " + (i + 1) + " disciplina = = = = ");
-			System.out.println((i + 1) + ". " + horasDiscSemestre[i]);
+			System.out.println((i + 1) + ". " + nomesDisc[i] + " - "+ horasDiscSemestre[i]);
 		}
 
 	}
