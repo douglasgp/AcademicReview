0dce3e54d4f420a60607590cfeda98d43f7dbebb
douglasgp <douglasgaldino22@hotmail.com>
2019-07-28 12:55:22 -0300

    Remoção de código depreciado

---
+++ src/br/com/review/controller/GeraArqDisciplina.java
@@ -0,0 +1,21 @@
+package br.com.review.controller;
+
+import java.util.Scanner;
+
+import br.com.review.model.Disciplinas;
+
+public class GeraArqDisciplina {
+	public static Scanner scan = new Scanner(System.in);
+	public static int TOTAL_DISC_SEMESTRE = 0;
+	public static String[] OBJ_DISC = new String[TOTAL_DISC_SEMESTRE];
+	public static String[] EMENTA_DISC = new String[TOTAL_DISC_SEMESTRE];
+	public static String[] BIBLIO_DISC = new String[TOTAL_DISC_SEMESTRE];
+	
+	public static int[] CARGA_DISC = new int[TOTAL_DISC_SEMESTRE];
+	public static String[] NOME_DISC = new String[TOTAL_DISC_SEMESTRE];
+	
+	public static void disciplinaSemestre() {
+		
+		
+	}
+}
