2280724a4060898f883ab580e7e97ca75f5cf73f
douglasgp <douglasgaldino22@hotmail.com>
2019-07-28 12:46:00 -0300

    Método vetor de String
    Reorganização de método com retorno de vetor contendo nomes das
    disciplinas de cada semestre

--- src/br/com/review/controller/MethodsReview.java
+++ src/br/com/review/controller/MethodsReview.java
@@ -1,7 +1,10 @@
 package br.com.review.controller;
 
+import br.com.review.model.Bibliografia;
+
 public class MethodsReview {
-	
+	public static int contBibli = 0;
+
 	// Retorna total de disciplinas do semestre selecionado
 	public static int retornaTotalDisciplinas(int opcao) {
 		int totalDisciplina = 0;
@@ -50,5 +53,52 @@ public class MethodsReview {
 			}
 		}
 		return mHoraAula;
+	}
+
+	// Método retorna nomes das disciplinas do semestre selecinado
+	public static String[] disciplinasSemestral(int semestre, int qtdDisc) {
+		String disciplinas[] = new String[qtdDisc];
+		if (semestre == 1) {
+			String disciplinaSem1[] = { "ILM001", "ISI002", "IAL002", "IAC001", "AAG001", "MMD001", "LPO001",
+					"LIN100" };
+			for (int i = 0; i < disciplinaSem1.length; i++) {
+				disciplinas[i] = disciplinaSem1[i];
+			}
+		} else if (semestre == 2) {
+			String disciplinaSem2[] = { "IES100", "ILP010", "ILP502", "IHW100", "ISO100", "MET100", "MCA002", "CCG006",
+					"LIN200" };
+			for (int i = 0; i < disciplinaSem2.length; i++) {
+				disciplinas[i] = disciplinaSem2[i];
+			}
+		} else if (semestre == 3) {
+			String disciplinaSem3[] = { "IES200", "IED001", "IHC001", "ILP503", "ISO200", "IBD002", "MPL001", "CEF100",
+					"LIN300" };
+			for (int i = 0; i < disciplinaSem3.length; i++) {
+				disciplinas[i] = disciplinaSem3[i];
+			}
+		} else if (semestre == 4) {
+			String disciplinaSem3[] = { "IES300", "ILP007", "IBD100", "IRC008", "AGO006", "ISG003", "HST002",
+					"LIN400" };
+			for (int i = 0; i < disciplinaSem3.length; i++) {
+				disciplinas[i] = disciplinaSem3[i];
+			}
+		}
+		return disciplinas;
+	}
+
+	// Método retorna descrição das disciplinas
+	public static String[] descricaoDisciplinas(int semestre, int qtdDisc) {
+		// String biblio = "biblio"+contBibli;
+		
+		do {
+			
+		} while (condition);
+		Bibliografia[] biblio = new Bibliografia[contBibli];
+		if(semestre == 1) {
+			Bibliografia[] biblio = new Bibliografia("BROWN, C. E.; PETRUSCA, R.","Programando em ACCESS com VBA", "Alta Books", 2006);
+		} else if(semestre == 2) {
+			
+		}
+		
 	}
 }
