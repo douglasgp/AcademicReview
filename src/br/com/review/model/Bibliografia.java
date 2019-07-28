0ec702dc4e16a6972b10bce261c808274d9ac210
douglasgp <douglasgaldino22@hotmail.com>
2019-07-28 12:48:58 -0300

    Criação de variáveis e métodos de acesso

---
+++ src/br/com/review/model/Bibliografia.java
@@ -0,0 +1,49 @@
+package br.com.review.model;
+
+public class Bibliografia {
+
+	private String titulo;
+	private String autor;
+	private String editora;
+	private int ano;
+	
+	public Bibliografia(String bAutor, String bTitulo, String bEditora, int bAno) {
+		autor = bAutor;
+		titulo = bTitulo;
+		editora = bEditora;
+		ano = bAno;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+}
