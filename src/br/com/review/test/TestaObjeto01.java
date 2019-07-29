ed0759465cede36b729a01033a5221a5df64d95c
douglasgp <douglasgaldino22@hotmail.com>
2019-07-29 00:02:54 -0300

    Configura menu de seleção para escolha de datas das aulas

--- src/br/com/review/test/TestaObjeto01.java
+++ src/br/com/review/test/TestaObjeto01.java
@@ -10,6 +10,9 @@ import br.com.review.model.Disciplinas;
 public class TestaObjeto01 {
 	public static Scanner scan = new Scanner(System.in);
 	public static int TOTAL_HORAS_SEMESTRE = 0;
+	public static int TOTAL_DISCIPLINAS = 0;
+	public static String[] NOMES_DISCIPLINAS = new String[TOTAL_DISCIPLINAS];
+	public static int[] DISC_SEM_ATUAL = new int[TOTAL_DISCIPLINAS];
 	
 	public static void main(String[] args) {
 		menuPrincipal();
@@ -19,24 +22,37 @@ public class TestaObjeto01 {
 		System.out.println("Informe o semestre: ");
 		int semestre = scan.nextInt();
 
-		int totalDiscSemestre = MethodsReview.retornaTotalDisciplinas(semestre);
+		TOTAL_DISCIPLINAS = MethodsReview.retornaTotalDisciplinas(semestre);
 
-		int horasDiscSemestre[] = MethodsReview.cargaHorariaSemestre(semestre, totalDiscSemestre);
+		int horasDiscSemestre[] = MethodsReview.cargaHorariaSemestre(semestre, TOTAL_DISCIPLINAS);
 		
-		String[] nomesDisc = MethodsReview.disciplinasSemestral(semestre, totalDiscSemestre);
+		String[] nomesDisc = MethodsReview.disciplinasSemestral(semestre, TOTAL_DISCIPLINAS);
+
+		System.out.println("Qtd disciplinas: " + TOTAL_DISCIPLINAS);
 		
-		System.out.println("Qtd disciplinas: " + totalDiscSemestre);
 		for (int i = 0; i < horasDiscSemestre.length; i++) {
-			System.out.println((i + 1) + ". " + nomesDisc[i] + " - " + horasDiscSemestre[i]);
+			System.out.println((i + 1) + ". " + nomesDisc[i] + " - " + horasDiscSemestre[i] + " hs");
 			TOTAL_HORAS_SEMESTRE += horasDiscSemestre[i];
 		}
+		System.out.println("Total Horas: " + TOTAL_HORAS_SEMESTRE);
+		int opcao = 0;
+		int selecionaDisc = 0;
+		int cont = 0;
+		do {
+			System.out.println("Escolha a " + (cont+1)+" disciplina:");
+			selecionaDisc = scan.nextInt();
+			
+			DISC_SEM_ATUAL[cont] = scan.nextInt();
+			
+			cont++;
+		} while (opcao == 2);
 	}
 	
 	public static void menuPrincipal() {
 		int opcao = 0;
 		do {
-			System.out.println(" = = = = = = = = = = = = = = = MENU PRINCIPAL = = = = = = = = = = = = = = = ");
-			System.out.printf("%14.12s | %-14.13s | %-20.25s | %-17.20s | %-17.10s | %s\n", "1. Registrar Aula","2. NÃO CONFIGURADO ", "3. NÃO CONFIGURADO", "4. NÃO CONFIGURADO", "5. NÃO CONFIGURADO", "0. Finalizar");
+			System.out.println(" = = = = = = = = = = = = = = = MENU PRINCIPAL = = = = = = = = = = = = = = = \n");
+			System.out.printf("%14.18s | %-14.18s | %-20.25s | %-17.20s | %-17.10s | %s\n", "1. Registrar Aula","2. Consultar Registro ", "3. NÃO CONFIGURADO", "4. NÃO CONFIGURADO", "5. NÃO CONFIGURADO", "0. Finalizar");
 			opcao = scan.nextInt();
 			switch (opcao) {
 			case 1:
