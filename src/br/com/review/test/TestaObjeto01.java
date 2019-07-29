331549268e0c77c91d56f8081f76974d7aca7b59
douglasgp <douglasgaldino22@hotmail.com>
2019-07-28 23:25:51 -0300

    Prorrogar implementação de bibliografia

--- src/br/com/review/test/TestaObjeto01.java
+++ src/br/com/review/test/TestaObjeto01.java
@@ -9,8 +9,13 @@ import br.com.review.model.Disciplinas;
 
 public class TestaObjeto01 {
 	public static Scanner scan = new Scanner(System.in);
-	public static int contBiblio = 0;
+	public static int TOTAL_HORAS_SEMESTRE = 0;
+	
 	public static void main(String[] args) {
+		menuPrincipal();
+	}
+	
+	public static void carregaSemestre() {
 		System.out.println("Informe o semestre: ");
 		int semestre = scan.nextInt();
 
@@ -20,16 +25,30 @@ public class TestaObjeto01 {
 		
 		String[] nomesDisc = MethodsReview.disciplinasSemestral(semestre, totalDiscSemestre);
 		
-		Bibliografia[] biblio = new Bibliografia[contBiblio];
-		
-		
 		System.out.println("Qtd disciplinas: " + totalDiscSemestre);
 		for (int i = 0; i < horasDiscSemestre.length; i++) {
-			System.out.println((i + 1) + ". " + nomeDiscSemestre[i] + " - " + horasDiscSemestre[i]);
+			System.out.println((i + 1) + ". " + nomesDisc[i] + " - " + horasDiscSemestre[i]);
 			TOTAL_HORAS_SEMESTRE += horasDiscSemestre[i];
-			System.out.println((i + 1) + ". " + nomesDisc[i] + " - "+ horasDiscSemestre[i]);
 		}
-
+	}
+	
+	public static void menuPrincipal() {
+		int opcao = 0;
+		do {
+			System.out.println(" = = = = = = = = = = = = = = = MENU PRINCIPAL = = = = = = = = = = = = = = = ");
+			System.out.printf("%14.12s | %-14.13s | %-20.25s | %-17.20s | %-17.10s | %s\n", "1. Registrar Aula","2. NÃO CONFIGURADO ", "3. NÃO CONFIGURADO", "4. NÃO CONFIGURADO", "5. NÃO CONFIGURADO", "0. Finalizar");
+			opcao = scan.nextInt();
+			switch (opcao) {
+			case 1:
+				carregaSemestre();
+				break;
+			case 2:
+				System.out.println("Não configurado");
+				break;
+			default:
+				break;
+			}
+		} while (opcao == 0);
 	}
 
 }
