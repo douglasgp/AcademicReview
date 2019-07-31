ee4869005361fba0f72d0e7129abb784790d05b4
douglasgp <douglasgaldino22@hotmail.com>
2019-07-30 22:59:33 -0300

    Cria classe principal para chamar vetor de bibliografia

---
+++ src/br/com/review/test/TestaBiblio.java
@@ -0,0 +1,11 @@
+package br.com.review.test;
+
+import br.com.review.model.Bibliografia;
+
+public class TestaBiblio {
+	public static void main(String[] args) {
+		
+		//Bibliografia[] biblio = {"Alberto", bTitulo, bEditora, bAno};
+	}
+
+}
