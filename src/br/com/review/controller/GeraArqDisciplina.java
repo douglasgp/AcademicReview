ed757071f70dbd91ebcc479d29c2d85f6bc239f3
douglasgp <douglasgaldino22@hotmail.com>
2019-07-30 23:01:00 -0300

    Alteração na referência no nome do método

--- src/br/com/review/controller/GeraArqDisciplina.java
+++ src/br/com/review/controller/GeraArqDisciplina.java
@@ -16,7 +16,7 @@ public class GeraArqDisciplina {
 		int qtdDisc = MethodsReview.retornaTotalDisciplinas(semestre);
 		
 		// int[] referencia = {00+semestre, 01+semestre};
-		String[] sigla = MethodsReview.disciplinasSemestral(semestre, qtdDisc);
+		String[] sigla = MethodsReview.siglaDisc(semestre, qtdDisc);
 		String[] denominacao = MethodsReview.denominacaoDisc(semestre, qtdDisc);
 		int[] cargaHs = MethodsReview.cargaHorariaSemestre(semestre, qtdDisc);
 				
